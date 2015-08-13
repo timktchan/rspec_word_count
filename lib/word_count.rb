1824c25d1eee6bdcf1f95aa589baec3e5c3a73be
timktchan <chankingto819@gmail.com>
2015-08-13 10:21:02 +0800

    tim_rspec_word_count

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,27 @@
 class String
-  def unique_word_count
+  attr_reader :user_phase, :user_arr, :unique_phase, :unique_count
+  
+  def unique_word_count 
+    @user_arr = self.split(',')
+    @unique_phase = @user_arr
+    @unique_count = @user_arr.length - @unique_phase.length
+    puts @unique_count
   end
   
   def word_frequency_count
+
   end  
+
+  def question (phrase)
+    puts "Enter a phrase"
+    @user_phrase = gets
+    @user_phrase.unique_word_count
+    @user_phrase.word_frequency_count
+  end
+
+  def reply
+    puts "Number of unique words: #{user_phrase.unique_word_count}"
+    puts "Word counts: #{user_phrase.word_frequency_count}"
+  end
 end
+
